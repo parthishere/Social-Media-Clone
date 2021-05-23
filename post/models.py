186ea895c681683d379aac5c1622cd8df9293d0e
parthishere <parthishere1234@gmail.com>
2021-05-23 22:07:36 +0530

    build: add post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post/images')
+    likes = models.ForeignKey(User, verbose_name='liked_user', on_delete=models.CASCADE, null=True, blank=True)
+    like_count = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    class Meta():
+        ordering = ['-id']
