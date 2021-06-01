be17863712264d73c872fe924b45bea35e23c843
parthishere <parthishere1234@gmail.com>
2021-06-02 02:11:03 +0530

    feat: change model manager of post app

--- post/models.py
+++ post/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class PostManager(models.Manager):
+    
+    def show_likes_by_followings(self,request,post=None):
+        user = request.user
+        likes_by_followings = []
+        if user.is_authenticated:
+            try:
+                user_obj = self.model.objects.get(user=user)
+            except Exception as e:
+                print(e) 
+            following_user = user_obj.return_following
+            for u in list(post.likes.objects.all()):
+                if u in following_user:
+                   likes_by_followings.append(u)
+        return likes_by_followings 
+                    
+            
+            
+
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
