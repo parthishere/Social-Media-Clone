81463c4ecc40530673aeec364b2fd88dc69b6e02
parthishere <parthishere1234@gmail.com>
2021-06-02 00:38:41 +0530

    change model manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from post.models import Post
 from django.db import models
 from django.db.models.signals import post_save, pre_save, m2m_changed
 from django.utils.text import slugify
@@ -27,6 +28,34 @@ def unique_slug_generator_for_user_profile(instance, new_slug=None):
     return slug
 
 
+class UserProfileManager(models.Manager):
+    def get_or_new(self, request):
+        obj = None
+        created = False
+        try: 
+            qs = self.model.objects.filter(user=request.user)
+        except Exception as e:
+            print(e)
+        if qs.count() == 1:
+            obj = qs.first()
+            created = False
+        else:
+            obj = self.model.objects.create(user=request.user)
+            created = True
+        return obj, created
+    
+    def get_liked_posts(request):
+        user = request.user
+        post_count = 0
+        if user.is_authenticated:
+            qs = Post.objects.filter(likes=user)
+            for obj in qs:
+                post_count +=1
+            return qs, post_count
+        
+
+                
+
 
 
 class UserProfile(models.Model):
@@ -44,6 +73,7 @@ class UserProfile(models.Model):
     topic = models.CharField(max_length=100, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     verified = models.BooleanField(default=False)
+    saved   = models.ManyToManyField(Post, blank=True, null=True)
     
     class Meta():
         ordering = ['-id']
@@ -55,7 +85,7 @@ class UserProfile(models.Model):
         return reverse("model_detail", kwargs={"slug": self.slug})
     
     def check_verified(self):
-        if self.user.is_authenticated and self.follower_count >= 100:
+        if self.user.is_authenticated and self.follower_count >= 100 and self.total_likes > 1000:
             if self.post > 10 and (self.timestamp[:4]-timezone.now()[:4]) >= 3:
                 self.verified = True
                 self.save()
