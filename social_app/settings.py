7d517f7e8c346839b9b73f95ebca988ac2495506
parthishere <parthishere1234@gmail.com>
2021-06-22 03:19:23 +0530

    change in settings

--- social_app/settings.py
+++ social_app/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devmediaone.herokuapp.com', '1227.0.0.1']
 
 
 # Application definition
