6c2c353a67088a8dd027435cbd43d4e639388e69
parthishere <parthishere1234@gmail.com>
2021-06-29 01:54:38 +0530

    learn : chat app

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,1 +1,12 @@
-from channels.generic.websocket import AsyncWebSocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatRoomConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        
+        await self.channel_layer.group_add (
+            self.room_group_name,
+            self.channel_name
+        )
--- social_app/settings.py
+++ social_app/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     
+    'chat',
     'accounts',
     'post',
     'comments',
