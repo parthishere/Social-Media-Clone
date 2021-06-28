5c173d23257dfba9f0f93c0b50ba466fcf17548c
parthishere <parthishere1234@gmail.com>
2021-06-29 02:43:15 +0530

    learning channel

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,5 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+import json
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -7,6 +7,32 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         self.room_group_name = f'chat_{self.room_name}'
         
         await self.channel_layer.group_add (
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": 'text_massage',
+                'massage': "hello world"
+            } 
+        )
+       
+        
+    async def tester_massage(self, event):
+        tester = event['massage']
+        
+        await self.send(text_data = json.dumps({
+            'tester':tester
+            })
+        )
+     
+
+        
+    async def disconnect(self, close_code):
+        
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
