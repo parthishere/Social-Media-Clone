14c50c4d374d6317701400716d0bc68b1d3e3989
parthishere <parthishere1234@gmail.com>
2021-07-29 22:20:04 +0530

    change in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,13 +17,15 @@ from .views import (
 app_name = 'accounts'
 
 urlpatterns = [
+    path('follow-requests/', follow_requested_user_list, name='followers-requests'),
     path('list/', UserSearchListView.as_view(), name='list'),
-    path('requests/', follow_requested_user_list, name='followers-requests'),
-    path('<username>/', UserProfileDetailView.as_view(), name='profile'),
-    path('<username>/update/', user_profile_update, name='update'),
-    path('<username>/followers/', user_followers_list, name='user-followers'),
-    path('<username>/followings/', user_following_list, name='user-following'),
-    path('follow/<username>/', follow_unfollow_requested_user, name='follow-requested-user'),
-    path('accept-request/<username>/', accept_follow_request_view, name='accept-request'),
-    path('decline-request/<username>/', decline_follow_request_view, name='decline-request'),
+    
+    
+    path('<str:username>/update/', user_profile_update, name='update'),
+    path('<str:username>/followers/', user_followers_list, name='user-followers'),
+    path('<str:username>/followings/', user_following_list, name='user-following'),
+    path('follow/<str:username>/', follow_unfollow_requested_user, name='follow-requested-user'),
+    path('accept-request/<str:username>/', accept_follow_request_view, name='accept-request'),
+    path('decline-request/<str:username>/', decline_follow_request_view, name='decline-request'),
+    path('<str:username>', UserProfileDetailView.as_view(), name='profile'),
 ]
